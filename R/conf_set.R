25b666b562ae9e718966ddaea352aa136ee628d6
ArslanBatyrov <batyrovarslan7@gmail.com>
2026-07-04 14:43:49 +0100

    Replace PELT_CS scaffold with confidence_set() entry point, was done due to the decision to use PELTC and make changes to it, thus the empty space for the C will be allocated withing the PELT function not conf_set.R

--- R/conf_set.R
+++ R/conf_set.R
@@ -1,37 +1,45 @@
-# for now I am creating the empty function that will be able to receive
-# data from the C execution; for now it is empty as the C execution is not implemented.
-# I will test the code below with a simple C function that will return the value and
-# make sure that it does what it is supposed to.
+# Deleted the PELT_CS as the design changed and we need to work within PELTC.
+# conf_set, as agreed, should be just a function that takes all of the values
+# required to calculate the confidence sets and then calculates it.
+# Also, a little later on, I will make confint call this.
+# We will have two functions doing the same thing to make it more comfortable for users.
+# In the docs, we will describe conf_set only.
 
-PELT_CS = function(sumstat, pen = 0, cost_func = "mean.norm", shape = 1, minseglen = 1){
+confidence_set = function(object, level = 0.95) {
+  if(!is(object, "cpt")){stop("object must be a cpt or cpt.range, cpt.reg is not supported")}
+  if(object@method != "PELT"){stop("confidence sets are currently only implemented for method = 'PELT'")}
 
-  # As I do not know how many changepoints we have, I need to first run PELT to get the number
-  # of changepoints, so that I can allocate the required space for each.
-  pelt = PELT(sumstat, pen = pen, cost_func = cost_func, shape = shape, minseglen = minseglen)
+  # Turning the human-readable test.stat into a C-readable cost-function key
+  if(test.stat(object) != "Normal"){stop("confidence set supports test.stat = 'Normal' only")}
+  costfunc = switch(cpttype(object),
+                    "mean" = "mean.norm",
+                    "variance" = "var.norm",
+                    "mean and variance" = "meanvar.norm",
+                    stop("Unsupported changepoint type: ", cpttype(object)))
+  if(pen.type(object) == "MBIC"){costfunc = paste0(costfunc, ".mbic")}
 
-  n = length(sumstat[, 1]) - 1
-  n_cpts = length(pelt$cpts)
+  # I copied the sumstat formula from data_input.R for use here
+  data = data.set(object)
+  mu = mean(data)
+  sumstat = cbind(c(0, cumsum(coredata(data))), c(0, cumsum(coredata(data)^2)), cumsum(c(0, (coredata(data) - mu)^2)))
 
-  # my rationale (as we discussed with Rebecca) is that per changepoint I will allocate
-  # the max memory that could potentially be required, which is equal to the number of
-  # data points overall. As such, I find the number of changepoints and multiply them by
-  # the number of data points. Just in case, I pad unused columns with 0.
-  # So the data space would be n_cpts * n columns. (n_cpts is always >= 1.)
+  # basic validation of the level here, C does not see it for now. In C (PELTC) we only have an off/on button
+  if(!is.numeric(level) || length(level) != 1 || is.na(level) || level <= 0 || level >= 1){
+    stop("level must be a single number strictly between 0 and 1") # this is not used by C yet
+  }
 
-  checklist_positions = matrix(0L, nrow = n_cpts, ncol = n) # candidate changepoint locations
-  checklist_likes = matrix(0, nrow = n_cpts, ncol = n) # their (pure) likelihood values
+  # Re-run PELT with the conf_set on
+  pelt = PELT(sumstat, pen = pen.value(object), cost_func = costfunc, minseglen = object@minseglen, conf.set = TRUE)
 
-  # Label each bucket with the exact C type it will hold.
-  storage.mode(checklist_positions) = "integer"
-  storage.mode(checklist_likes) = "double"
-
-  # here I will add a C call later on,
-  # so that afterwards we can return the now-filled buckets back out of answer.
-
+  # just a sanity check that the re-run agrees with the changepoints we already stored
+  if(!all(cpts(object) %in% pelt$cpts)){
+    warning("re-run PELT did not reproduce the object's changepoints; confidence set may not correspond to the fit")
+  }
   return(list(
+    level = level,
     cpts = pelt$cpts,
-    lastchangecpts = pelt$lastchangecpts, # backpointer table
-    checklist_positions = checklist_positions, # empty space for C to fill
-    checklist_likes = checklist_likes # empty space for C to fill
+    lastchangecpts = pelt$lastchangecpts,
+    checklist_positions = pelt$checklist_positions, # fake pattern for now, will be real later
+    checklist_likes = pelt$checklist_likes # fake pattern as well for now
   ))
 }

73eb1993fc7236e4ab555e28a4737b35066b4ed8
ArslanBatyrov <batyrovarslan7@gmail.com>
2026-07-12 17:27:00 +0100

    Rewrite PELT driver with capacity-in/needed-out buffers and tryCatch retry(tryCatch is my own idea, must ask Rebecca and Owen whether it makes sense instead of calculating 50% of their available memory)

--- R/PELT_one_func_minseglen.R
+++ R/PELT_one_func_minseglen.R
@@ -30,28 +30,55 @@ PELT = function(sumstat,pen=0, cost_func = "norm.mean", shape = 1, minseglen = 1
   storage.mode(lastchangecpts) = 'integer'
   storage.mode(numchangecpts) = 'integer'
 
-  # empty space for the confidence-set execution in C to fill (only used when
-  # conf.set is TRUE, but .C always needs the same number of arguments).
-  # minseglen*minseglen, NOT n_cpts*n on a large dataset (I will need to chat with
-  # Owen and Rebecca as this way of allocating memory has too many crash edge cases)
-  # I will propose adding one more variable that both inputs to AND outputs from C:
-  # the first one is a guess, in case it is not enough we reallocate space with the
-  # exact number C requires
-  buflen = as.integer(minseglen) * as.integer(minseglen)
-  checklist_positions = rep(0L, buflen)
-  checklist_likes = rep(0, buflen)
-  storage.mode(checklist_positions) = 'integer'
-  storage.mode(checklist_likes) = 'double'
+  # conf_set output buffers for C will be allocated here
+  # the previous logic of minseglen^2 or n * ncpts both were inefficient
+  # So, currently the next method we are going to use is two fold
+  #  1: max(We allocate minseglen^2, 30) first
+  #  2: if it is enough -> Operation is done, if not:
+  #  3: C rewrites the cs_len and hands it to R, so that R knows exactly
+  #  the memory that is required to be allocated. Second run occurs
+  buflen = max(as.integer(minseglen) * as.integer(minseglen), 30L)
+
+  # one C call to PELTC, building a helper function to make runs
+  # simpler as we might call it twice in case our first guess memory allocation was wrong
+  # meaning cs_len < needed
+  run_peltc = function(buflen){
+    .C('PELTC', cost_func, sumstat, as.integer(n), as.double(pen), cptsout,
+       as.integer(error), as.double(shape), as.integer(minseglen),
+       lastchangelike, lastchangecpts, numchangecpts, conf.flag,
+       rep(0L, buflen), rep(0, buflen), as.integer(buflen))
+  }
 
   # answer=.C('PELT',cost_func, y3, y2,y,as.integer(n),as.double(pen),cptsout,as.integer(error),as.double(shape))
-  answer=.C('PELTC',cost_func, sumstat,as.integer(n),as.double(pen),cptsout,as.integer(error),as.double(shape), as.integer(minseglen), lastchangelike, lastchangecpts,numchangecpts, conf.flag, checklist_positions, checklist_likes)
+  answer = run_peltc(buflen)
 
   if(answer[[6]]>0){
     stop("C code error:",answer[[6]],call.=F)
   }
   if(conf.flag == 1){
-    # confidence set requested: also hand back the raw material C captured
-    return(list(lastchangecpts=answer[[10]],cpts=sort(answer[[5]][answer[[5]]>0]), lastchangelike=answer[[9]], ncpts=answer[[11]], checklist_positions=answer[[13]], checklist_likes=answer[[14]]))
+    needed = answer[[15]]          # count C reported it needed
+    captured = min(needed, buflen) # how many the first pass wrote
+    if(needed > buflen){
+      # If our first guess undershoot: reallocate to the exact size and re-run once.
+      # Wrap the possible too large allocation in tryCatch
+      # I did this as it was the way to make a universal upside cap
+      # that is simple, as if we were to try to check for % of total RAM available
+      # or what R has access to, we would make it excessively complicated and each device might need
+      # its own approach, while using tryCatch will give the same outcome
+      # ask whether this is a good idea in a convo with Rebecca and Owen
+      answer2 = tryCatch(run_peltc(needed), error = function(e){
+        warning("confidence-set buffer too large to allocate (", needed,
+                " slots); confidence set may be incomplete", call.=FALSE)
+        NULL
+      })
+      if(!is.null(answer2)){
+        if(answer2[[6]]>0){ stop("C code error:",answer2[[6]],call.=F) }
+        answer = answer2
+        captured = needed
+      }
+    }
+    # trim off the unused padding so R only sees the real captured entries
+    return(list(lastchangecpts=answer[[10]],cpts=sort(answer[[5]][answer[[5]]>0]), lastchangelike=answer[[9]], ncpts=answer[[11]], checklist_positions=answer[[13]][seq_len(captured)], checklist_likes=answer[[14]][seq_len(captured)]))
   }
   return(list(lastchangecpts=answer[[10]],cpts=sort(answer[[5]][answer[[5]]>0]), lastchangelike=answer[[9]], ncpts=answer[[11]]))
 

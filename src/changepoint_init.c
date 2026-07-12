d1aeb5363a728835bf732bfd049f1aa498d54e87
ArslanBatyrov <batyrovarslan7@gmail.com>
2026-07-12 17:27:57 +0100

    Register PELTC with 15 arguments to match new cs_len parameter(one more complexity added, sadly)

--- src/changepoint_init.c
+++ src/changepoint_init.c
@@ -8,7 +8,7 @@
 /* .C calls */
 extern void binseg(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void FreePELT(void *);
-extern void PELTC(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void PELTC(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void CptReg_Normal_PELT(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void Free_CptReg_Normal_PELT(void *);
 extern void CptReg_Normal_AMOC(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
@@ -17,7 +17,7 @@ extern void Free_CptReg_Normal_AMOC(void *);
 static const R_CMethodDef CEntries[] = {
     {"binseg",   (DL_FUNC) &binseg,   10},
     {"FreePELT", (DL_FUNC) &FreePELT,  1},
-    {"PELTC",     (DL_FUNC) &PELTC,     14},
+    {"PELTC",     (DL_FUNC) &PELTC,     15},
     {"CptReg_Normal_PELT", (DL_FUNC) &CptReg_Normal_PELT, 13},
     {"Free_CptReg_Normal_PELT", (DL_FUNC) &Free_CptReg_Normal_PELT, 1},
     {"CptReg_Normal_AMOC", (DL_FUNC) &CptReg_Normal_AMOC, 13},
